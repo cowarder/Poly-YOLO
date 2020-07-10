232e417391c1bd7fdf7d54e402b9a1be9cfc3a3e
cowarder <1615482151@qq.com>
2020-07-10 12:15:46 +0800

    yolo_loss

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import numpy as np
 import torch, torchvision
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.optim as optim
 from torch.utils.data import DataLoader
 from dataset import MyDataset
 from utils import parse_cfg
@@ -15,6 +16,7 @@ ANGLE_STEP = 15  # that means Poly-YOLO will detect 360/15=24 vertices per polyg
 NUM_ANGLES3 = int(360 // ANGLE_STEP * 3)
 NUM_ANGLES = int(360 // ANGLE_STEP)
 anchors_per_level = 9
+grid_size_multiplier = 4
 
 
 class ChannelSELayer(nn.Module):
@@ -235,6 +237,46 @@ class Darknet(nn.Module):
         return models
 
 
+def box_iou(b1, b2):
+    """Return iou tensor
+
+    Parameters
+    ----------
+    b1: tensor, shape=(i1,...,iN, 4), xywh
+    b2: tensor, shape=(j, 4), xywh
+
+    Returns
+    -------
+    iou: tensor, shape=(i1,...,iN, j)
+
+    """
+    # Expand dim to apply broadcasting.
+    b1 = torch.unsqueeze(b1, -2)
+    b1_xy = b1[..., :2]
+    b1_wh = b1[..., 2:4]
+    b1_wh_half = b1_wh / 2.
+    b1_mins = b1_xy - b1_wh_half
+    b1_maxes = b1_xy + b1_wh_half
+
+    # Expand dim to apply broadcasting.
+    b2 = torch.unsqueeze(b2, 0)
+    b2_xy = b2[..., :2]
+    b2_wh = b2[..., 2:4]
+    b2_wh_half = b2_wh / 2.
+    b2_mins = b2_xy - b2_wh_half
+    b2_maxes = b2_xy + b2_wh_half
+
+    intersect_mins = torch.max(b1_mins, b2_mins)
+    intersect_maxes = torch.min(b1_maxes, b2_maxes)
+    intersect_wh = torch.max(intersect_maxes - intersect_mins, torch.tensor(0.))
+    intersect_area = intersect_wh[..., 0] * intersect_wh[..., 1]
+    b1_area = b1_wh[..., 0] * b1_wh[..., 1]
+    b2_area = b2_wh[..., 0] * b2_wh[..., 1]
+    iou = intersect_area / (b1_area + b2_area - intersect_area)
+
+    return iou
+
+
 def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
     """Convert final layer features to bounding box parameters.
     feats: nB, nC, nH, nW
@@ -242,7 +284,7 @@ def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
 
     nA = anchors_per_level
     nB, nC, nH, nW = feats.shape
-    anchors = torch.from_numpy(anchors).view([1, 1, 1, nA, 2])
+    anchors = anchors.view([1, 1, 1, nA, 2])
     grid_y = torch.linspace(0, nH-1, nH).view(-1, 1, 1, 1).repeat([1, nW, 1, 1])
     grid_x = torch.linspace(0, nW-1, nW).view(1, -1, 1, 1).repeat([nH, 1, 1, 1])
     grid = torch.cat([grid_x, grid_y], dim=-1)
@@ -265,7 +307,7 @@ def yolo_head(feats, anchors, num_classes, input_shape, calc_loss=False):
     b = torch.sum(a)
     diagonal = torch.sqrt(b)
     polygons_x = polygons_x * d / diagonal
-    polygons_y = feats[..., 5 + num_classes + 1:num_classes + 5 + NUM_ANGLES3:3]
+    polygons_y = feats[:, :, :, :, 5 + num_classes + 1:num_classes + 5 + NUM_ANGLES3:3]
     polygons_y = polygons_y.sigmoid()
     if calc_loss:
         return grid, feats, box_xy, box_wh, polygons_confidence
@@ -288,17 +330,84 @@ def yolo_loss(yolo_outputs, y_true, anchors, num_classes, ignore_thresh=.5):
     loss: tensor, shape=(1,)
 
     """
+    anchors = torch.from_numpy(anchors).float()
     g_y_true = y_true
-    input_shape = np.array(yolo_outputs.shape[2:])
-    grid_shapes = np.array(y_true[1:3])
+    input_shape = np.array(yolo_outputs.shape[2:]) * grid_size_multiplier
+
+    grid_shapes = np.array(y_true.shape[1:3])
     # batch size
-    N = yolo_outputs.shape[0]
+    nB = yolo_outputs.shape[0]
     ix = torch.LongTensor(range(5+num_classes+NUM_ANGLES3))
     object_mask = y_true.index_select(4, ix[4:5])   # confidence
     vertices_mask = y_true.index_select(4, ix[5 + num_classes + 2:5 + num_classes + NUM_ANGLES3:3])
     true_class_probs = y_true.index_select(4, ix[5:5 + num_classes])
     yolo_head(yolo_outputs, anchors, num_classes, input_shape, calc_loss=True)
-    pass
+
+    grid, raw_pred, pred_xy, pred_wh, pol_cnf = yolo_head(yolo_outputs, anchors, num_classes, input_shape, calc_loss=True)
+    pred_box = torch.cat([pred_xy.float(), pred_wh.float()], -1)
+    raw_true_xy = y_true[:, :, :, :, :2] * grid_shapes[::-1] - grid
+    raw_true_polygon0 = y_true[:, :, :, :, 5 + num_classes: 5 + num_classes + NUM_ANGLES3]
+
+    raw_true_wh = torch.log(y_true[..., 2:4] / anchors * input_shape[::-1]).float()
+    raw_true_wh[raw_true_wh==-float('inf')] = 0
+    # raw_true_wh = K.switch(object_mask, raw_true_wh, K.zeros_like(raw_true_wh))  # avoid log(0)=-inf
+
+    raw_true_polygon_x = raw_true_polygon0[..., ::3]
+    raw_true_polygon_y = raw_true_polygon0[..., 1::3]
+
+    dx = torch.square(anchors[..., 0:1] / 2)
+    dy = torch.square(anchors[..., 1:2] / 2)
+    d = torch.sqrt(dx + dy)
+    # a = torch.pow(torch.from_numpy(np.array(input_shape[::-1][0])), 2).float()
+    a = torch.pow(torch.tensor(input_shape[::-1][0]), 2).float()
+    # b = torch.pow(torch.from_numpy(np.array(input_shape[::-1][1])), 2).float()
+    b = torch.pow(torch.tensor(input_shape[::-1][1]), 2).float()
+    diagonal = torch.sqrt(a + b)
+    raw_true_polygon_x = torch.log(raw_true_polygon_x / d * diagonal)
+    raw_true_polygon_x[raw_true_polygon_x == -float('inf')] = 0
+    # raw_true_polygon_x = K.switch(vertices_mask, raw_true_polygon_x, K.zeros_like(raw_true_polygon_x))
+    box_loss_scale = 2 - y_true[..., 2:3] * y_true[..., 3:4]
+
+    # Find ignore mask, iterate over each of batch.
+    ignore_mask = []
+    object_mask_bool = object_mask.bool()
+    for b in range(nB):
+        true_box = y_true[b, ..., 0:4][object_mask_bool[b, ..., 0]]
+        iou = box_iou(pred_box[b], true_box)    # H,W, nA, num_gt
+        best_iou, _ = torch.max(iou, -1)
+        ignore_mask.append(best_iou < ignore_thresh)
+
+    ignore_mask = torch.stack(ignore_mask)
+    ignore_mask = ignore_mask.unsqueeze(-1)
+    # K.binary_crossentropy is helpful to avoid exp overflow.
+    xy_loss = object_mask * box_loss_scale * F.binary_cross_entropy_with_logits(raw_pred[..., 0:2], raw_true_xy)
+    wh_loss = object_mask * box_loss_scale * 0.5 * torch.square(raw_true_wh - raw_pred[..., 2:4])
+
+    # print((1-object_mask).shape)
+    # print(F.binary_cross_entropy_with_logits(object_mask, raw_pred[..., 4:5]))
+    # print(((1 - object_mask) * F.binary_cross_entropy_with_logits(object_mask, raw_pred[..., 4:5]) * ignore_mask).shape)
+
+    confidence_loss = object_mask * F.binary_cross_entropy_with_logits(raw_pred[..., 4:5], object_mask)
+    # + (1 - object_mask) * F.binary_cross_entropy_with_logits(raw_pred[..., 4:5], object_mask) * ignore_mask
+
+    class_loss = object_mask * F.binary_cross_entropy_with_logits(raw_pred[..., 5:5 + num_classes], true_class_probs)
+    polygon_loss_x = object_mask * vertices_mask * box_loss_scale * 0.5 * torch.square(
+        raw_true_polygon_x - raw_pred[..., 5 + num_classes:5 + num_classes + NUM_ANGLES3:3])
+    polygon_loss_y = object_mask * vertices_mask * box_loss_scale * F.binary_cross_entropy_with_logits(
+        raw_pred[..., 5 + num_classes + 1:5 + num_classes + NUM_ANGLES3:3], raw_true_polygon_y)
+    vertices_confidence_loss = object_mask * F.binary_cross_entropy_with_logits(
+        raw_pred[..., 5 + num_classes + 2:5 + num_classes + NUM_ANGLES3:3], vertices_mask)
+
+    xy_loss = torch.sum(xy_loss) / nB
+    wh_loss = torch.sum(wh_loss) / nB
+    class_loss = torch.sum(class_loss) / nB
+    confidence_loss = torch.sum(confidence_loss) / nB
+    vertices_confidence_loss = torch.sum(vertices_confidence_loss) / nB
+    polygon_loss = torch.sum(polygon_loss_x) / nB + torch.sum(polygon_loss_y) / nB
+
+    loss = (xy_loss + wh_loss + confidence_loss + class_loss + 0.2 * polygon_loss + 0.2 * vertices_confidence_loss) / (
+            torch.sum(object_mask) + 1) * nB
+    return loss
 
 
 def main():
@@ -340,17 +449,27 @@ def main():
     num_train = len(lines)
     batch_size = 2  # decrease/increase batch size according to your memory of your GPU
     print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-    gene = data_generator_wrapper(lines, batch_size, input_shape, anchors, num_classes, True)
 
     train_dataset = MyDataset(lines, input_shape, anchors, num_classes, True)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
     val_dataset = MyDataset(lines_val, input_shape, anchors, num_classes, False)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
-    for i, (image, labels) in enumerate(train_loader):
-        yolo_outputs = model(image)
-        yolo_loss(yolo_outputs, labels, anchors, num_classes, ignore_thresh=0.5)
-        break
+    epochs = 10
+    optimizer = optim.Adam(model.parameters(), lr=0.001)
+    for epoch in range(epochs):
+        running_loss = 0.0
+        for i, (image, labels) in enumerate(train_loader):
+            optimizer.zero_grad()
+            yolo_outputs = model(image)
+            loss = yolo_loss(yolo_outputs, labels, anchors, num_classes, ignore_thresh=0.5)
+            loss.backward()
+            optimizer.step()
+
+            running_loss += loss.item()
+            if i % 100 == 99:
+                print("epoch_{},iter_{}, loss_{}".format(epoch, i, running_loss / 100))
+                running_loss = 0.0
 
 
 if __name__=='__main__':
